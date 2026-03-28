d8b0efeefe6ab3cb396fe856731fe299cebe9b00
Ishant Awasthi <ishantawasthi350@gmail.com>
2026-03-28 21:26:42 +0530

    feat: add Task and User models for task management

---
+++ backend/models/Tasks.model.js
@@ -0,0 +1,30 @@
+
+import mongoose from "mongoose";
+
+const TaskSchema = new mongoose.Schema({
+
+     title:{
+        type:String,
+        required:true
+     },
+
+     description:{
+        type:String,
+        required:true
+     },
+        status:{
+        type:String,
+        enum:["pending","completed"],
+        default:"pending"
+        },
+
+  //  ID of the user who created the task
+        user:{
+        type:mongoose.Schema.Types.ObjectId,  // This is a reference to the User model
+        ref:"User",
+        required:true
+     }
+        
+},{timestamps:true});
+
+export const Task=mongoose.model("Task",TaskSchema);
